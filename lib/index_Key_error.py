22670198ec89450ad0e35ccb177af6f22b805d90
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-12-05 19:37:42 +0300

    All test passed

---
+++ lib/index_Key_error.py
@@ -0,0 +1,15 @@
+list = [0, 1, 2, 3, 4,]
+dict = {'a': 1, 'b': 2, 'c': 3,}
+
+list[10]
+# IndexError: List index out of range
+
+dict['d']
+# Traceback(most recent call last):
+
+# A NameError arises when a name is referenced before it has been defined.
+
+#  flatiron_school
+# Traceback (most recent call last):
+# ", line 1, in <module>
+# NameError: name 'flatiron_school' is not defined
